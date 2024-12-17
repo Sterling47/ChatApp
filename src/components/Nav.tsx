03f40849eba57aa246e5a6f480ee9cafe7984f4a
Peter Kim <peterkim.pk1@gmail.com>
2024-12-16 19:23:03 -0500

    Added react-icons library. Imported IconContext to resize icon. Centered items in nav-menu-wrapper

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,9 +5,11 @@ import { useState, useEffect } from "react"
 import type {Room,User} from '@prisma/client'
 import CreateRoom from '@/components/CreateRoom';
 import Link from "next/link"
-import publicChat from '/public/public-chat-icon.png'
 import Image from "next/image"
 import { pusherClient } from '@/lib/pusher-client'
+import { MdOutlinePublic } from "react-icons/md";
+import { RiGitRepositoryPrivateFill } from "react-icons/ri";
+import { IconContext } from "react-icons";
 
 interface RoomProps {
   initialRooms: Room[]
@@ -30,7 +32,7 @@ const Nav:React.FC<RoomProps> = ({initialRooms,user}) => {
     <nav>
       <div className="user-bar">
         <button className="user-bttn" onClick={toggleModal}>
-          <h4>{user.email}</h4>
+          <h4 id='username'>{user.email}</h4>
         </button>
         {isModalOpen && (
           <div className="user-modal">
@@ -40,14 +42,16 @@ const Nav:React.FC<RoomProps> = ({initialRooms,user}) => {
           </div>
         )}
         <ul className='nav-menu-wrapper'>
-          <li> <Image className="icon" src={'/private-chat-icon.png'} 
-          width={100}
-          height={100}
-          alt="" /></li>
-          <li> <Image className="icon" src={"/public-chat-icon.png"} 
-          width={100}
-          height={100}
-          alt="" /></li>
+          <li> 
+            <IconContext.Provider value={{color: '#ff7f11', size: '1.4em', className:"chat-icon"}}>
+              <RiGitRepositoryPrivateFill/>
+            </IconContext.Provider>
+          </li>
+          <li>
+            <IconContext.Provider value={{color: '#ff7f11', size: '1.4em', className:"chat-icon"}}>
+             <MdOutlinePublic/>
+            </IconContext.Provider>
+          </li>
         </ul>
       </div>
       <div className="user-select-display"> 
