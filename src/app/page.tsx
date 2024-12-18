f80d294f5a7b51c5a481e6f81e34588c570d2eb6
Adam Konber <konber3@gmail.com>
2024-12-18 13:35:50 -0700

    Installed tailwind to use Magic Ui retro-grid componet for login-page background. Fixed DB issue not finding room id by finishing integrating prisma-postgres by migrating prisma schema to new non-local DB
    Co-authored-by: Peter Kim <peterkimpk1@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+
 import './globals.css'
 import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/components';
 import { LoginButton } from '@/components/LoginButton';
+import  RetroGrid from '@/components/ui/retro-grid'
 
 export default async function App() {
   
@@ -8,6 +10,7 @@ export default async function App() {
     <div className='home'>
       <div className='login-box'>      
         <h1>Welcome to ChatApp</h1>
+        <RetroGrid />
         <LoginButton>
           <LoginLink className='l-link'>Sign In</LoginLink>
         </LoginButton>
