978a19fad4a4b2c42830bcd67bc17c987367678e
Peter Kim <161254279+peterkimpk1@users.noreply.github.com>
2025-01-13 15:37:16 -0500

    Removed 6 and turned p tag into div.
    Co-Authored-By: Adam Konber <konber3@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function App() {
   return (
     <div className='flex justify-center flex-col items-start h-[100vh]' id='home'> 
         <h1 className='ml-12 text-[6rem]'>Welcome to Chatt-r</h1>
-        <p className='m-[3rem] text-[2rem]'>Your place to <FlipWords words={words}/></p>
+        <div className='m-[3rem] text-[2rem]'>Your place to <FlipWords words={words}/></div>
         <LoginButton>
           <LoginLink className='z-auto hover:cursor-pointer'>Sign In</LoginLink>
         </LoginButton>
@@ -22,4 +22,3 @@ export default async function App() {
     </div>
   );
 }
-6
