4504281a24a482eb6ee15d51d966a086576e70b4
Peter Kim <161254279+peterkimpk1@users.noreply.github.com>
2025-01-23 17:29:50 -0500

    feat: Replaced Link with onClick button that runs a function to add roomIDs to the URL using useRouter. Filters out duplicate roomIDs

--- src/components/RoomList.tsx
+++ src/components/RoomList.tsx
@@ -2,18 +2,18 @@
 
 import { useState, useEffect } from "react"
 import type {Room} from '@prisma/client'
-import Link from "next/link"
 import { pusherClient }  from '@/lib/pusher-client'
 import { MdOutlinePublic } from "react-icons/md";
 import { RiGitRepositoryPrivateFill } from "react-icons/ri";
 import { IconContext } from "react-icons";
+import { useRouter } from "next/navigation";
 
 interface RoomProps {
   initialRooms: Room[]
 }
 const RoomList:React.FC<RoomProps> = ({initialRooms}) => {
   const [rooms, setRooms] = useState<Room[]>(initialRooms)
-
+  const router = useRouter();
   useEffect(() => {
     pusherClient.subscribe('rooms-channel')
     const createRoomHandler = (data: Room) => {
@@ -25,19 +25,32 @@ const RoomList:React.FC<RoomProps> = ({initialRooms}) => {
       pusherClient.unbind('rooms-created', createRoomHandler);
     }
    },[])
+   const joinRoom = (roomID: number) => {
+    const currentPath = window.location.pathname;
+    const existingRoomIDs = currentPath
+    .split('/')
+    .filter((segment) => segment !== '' && !isNaN(Number(segment)))
+    .map(Number)
+
+    if (!existingRoomIDs.includes(roomID)) {
+      existingRoomIDs.push(roomID);
+    }
+    const newURL = `/Home/${existingRoomIDs.join('/')}`
+    router.push(newURL)
+   }
 
   const renderRoomLink = (room: Room) => (
     <div className='flex' key={room.id}>
-    <IconContext.Provider value={{className:"text-[#ff7f11] w-6 h-6"}}>
-      {room.isPrivate ? <RiGitRepositoryPrivateFill /> : <MdOutlinePublic />}
-    </IconContext.Provider>
-    <Link 
-      className='text-grey no-underline ml-8 text-sm hover:text-[#ff7f11]' 
-      href={`/Home/${room.id}`}
-    >
+      <IconContext.Provider value={{className:"text-[#ff7f11] w-6 h-6"}}>
+        {room.isPrivate ? <RiGitRepositoryPrivateFill /> : <MdOutlinePublic />}
+      </IconContext.Provider>
+      <button 
+        className='text-grey no-underline ml-8 text-sm hover:text-[#ff7f11]' 
+        onClick={() => joinRoom(room.id)}
+      >
       {room.name}
-    </Link>
-  </div>
+      </button>
+    </div>
   ) 
 
   return (
