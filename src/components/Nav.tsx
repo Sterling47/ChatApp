89219699788401e6d310d105966116ac2a3843e5
Adam Konber <konber3@gmail.com>
2025-02-07 13:57:25 -0700

    Added a link to the nav modal that takes users to the Friends list as well as another Link to Search User page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from "react"
-import type {Room,User} from '@prisma/client'
+import type { Room, User } from '@prisma/client'
 import CreateRoom from '@/components/CreateRoom';
 import { SeedUser } from "./SeedUser"
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
@@ -12,7 +12,7 @@ interface RoomProps {
   initialRooms: Room[]
   currentUser: User
 }
-const Nav:React.FC<RoomProps> = ({initialRooms,currentUser}) => {
+const Nav: React.FC<RoomProps> = ({ initialRooms, currentUser }) => {
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [user, setUser] = useState<User | undefined>(currentUser);
   const { setActiveRoomId } = useActiveRoom();
@@ -32,11 +32,11 @@ const Nav:React.FC<RoomProps> = ({initialRooms,currentUser}) => {
       }
     }
     fetchUser();
-   },[])
-   const toggleSearch = () => {
+  }, [])
+  const toggleSearch = () => {
     setActiveRoomId('search');
     toggleModal();
-   }
+  }
   return (
     <nav className="flex flex-col justify-start m-0.5 rounded-md list-none col-span-1 row-start-1 row-end-13 overflow-hidden">
       <div className="flex flex-row justify-around m-0.5 rounded-md bg-primary">
@@ -45,22 +45,23 @@ const Nav:React.FC<RoomProps> = ({initialRooms,currentUser}) => {
         </button>
         {isModalOpen && (
           <div className="flex flex-col absolute top-10 left-2 bg-primary p-2 z-10">
-            <LogoutLink 
-            className="p-2 hover:bg-gray-100 rounded-lg transition-colors hover:text-black"
-            postLogoutRedirectURL={'/'}>Logout</LogoutLink>
+            <LogoutLink
+              className="p-2 hover:bg-gray-100 rounded-lg transition-colors hover:text-black"
+              postLogoutRedirectURL={'/'}>Logout</LogoutLink>
             <Link href="/Home/SearchUser"
-  className="p-2 hover:bg-gray-100 rounded-lg transition-colors hover:text-black" onClick={toggleSearch}>Search User</Link>
+              className="p-2 hover:bg-gray-100 rounded-lg transition-colors hover:text-black" onClick={toggleSearch}>Search User</Link>
+            <Link href="/Home/FriendsList"
+              className="p-2 hover:bg-gray-100 rounded-lg transition-colors hover:text-black" onClick={toggleSearch}>Friends</Link>
+<Link href="/Home/Settings"
+  className="p-2 hover:bg-gray-100 rounded-lg transition-colors hover:text-black" onClick={toggleSearch}>Settings</Link>
           </div>
         )}
       </div>
-      <RoomList initialRooms={initialRooms}/>
-      <CreateRoom/>
+      <RoomList initialRooms={initialRooms} />
+      <CreateRoom />
+
     </nav>
   )
 }
 
 export default Nav
-
-
-
- 
