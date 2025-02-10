ac20d6ba1dfed6b7d85c21ff928ba78b8f97b13f
Adam Konber <konber3@gmail.com>
2025-02-10 13:58:33 -0700

    Fixed Typo

--- src/components/FirstTimeSetup.tsx
+++ src/components/FirstTimeSetup.tsx
@@ -6,7 +6,7 @@ const FirstTimeSetup = () => {
   return (
     <div className="flex justify-center items-center w-full h-full absolute backdrop-blur-sm">
       <div className="flex justify-start flex-col items-center p-[2rem] w-[50%] h-[50%] rounded-lg bg-primary">
-        <h2 className="text-2xl font-bold mb-4">Let's get set up!</h2>
+        <h2 className="text-2xl font-bold mb-4">Lets get set up!</h2>
         <form action={initialUserSetup} className="flex h-[70%] flex-col justify-evenly items-center w-full">
           <label className="flex flex-col w-full">
             Create Username:
