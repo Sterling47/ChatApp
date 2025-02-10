d23fbbed982b5297dd3118e52e65a7027075c67e
Adam Konber <konber3@gmail.com>
2025-02-10 14:07:22 -0700

    remove error from catch block because im using next response error

--- src/app/api/search-user/route.ts
+++ src/app/api/search-user/route.ts
@@ -44,7 +44,7 @@ export async function GET(request: Request) {
     });
 
     return NextResponse.json({ users });
-  } catch (error) {
+  } catch {
     return NextResponse.json({ error: 'Failed to fetch users' }, { status: 500 });
   }
 }
