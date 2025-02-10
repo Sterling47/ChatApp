33eb1a0d75898bd240667de557288460b73ba7d1
Adam Konber <konber3@gmail.com>
2025-02-10 15:52:28 -0700

    Took away loading

--- src/app/Home/layout.tsx
+++ src/app/Home/layout.tsx
@@ -1,5 +1,5 @@
-import '../globals.css';
-import Nav from '@/components/Nav';
+import '../globals.css'
+import Nav from '@/components/Nav'
 import prisma from '@/lib/db';
 import FirstTimeSetup from '@/components/FirstTimeSetup';
 import { SeedUser } from '@/components/SeedUser';
@@ -11,21 +11,18 @@ export default async function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   const room = await prisma.room.findMany();
   const user = await SeedUser();
-
-  if (!user) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <ActiveRoomProvider>
       <UserProvider user={user}>
-        <div className="grid h-screen grid-cols-9 grid-rows-12 p-0.25">
-          <Nav initialRooms={room} />
+        <div className='grid h-screen grid-cols-9 grid-rows-12 p-0.25'>
+          <Nav initialRooms={room}/>
           {user.isFirstLogin ? <FirstTimeSetup /> : children}
         </div>
       </UserProvider>
     </ActiveRoomProvider>
-  );
+  )
 }
