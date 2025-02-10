e907837bf150332ee0a68d33acd6cfe1cad8c684
Adam Konber <konber3@gmail.com>
2025-02-10 15:26:23 -0700

    Updated delete route to export properly
    Co-authored-by: Peter Kim <peterkimpk1@users.noreply.github.com>

--- src/app/api/deleteRoom/route.ts
+++ src/app/api/deleteRoom/route.ts
@@ -1,18 +1,14 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function DELETE(request:NextRequest) {
+export async function DELETE(request: NextRequest) {
   try {
-    const {roomId} = await request.json()
-    const deleteRoom = prisma.room.delete({
-      where: {id: roomId}
-    })
-    if (!deleteRoom) {
-      throw new Error ('Could not find room to delete')
-    }
-    return NextResponse.json(deleteRoom, {status: 200})
-  }
-  catch (err) {
-    return NextResponse.json({error: 'Failed to delete room', err},{ status: 500 })
+    const { roomId } = await request.json();
+    const deleteRoom = await prisma.room.delete({
+      where: { id: roomId },
+    });
+    return NextResponse.json(deleteRoom, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ error: 'Failed to delete room', err }, { status: 500 });
   }
 }
