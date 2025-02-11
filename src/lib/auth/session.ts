b4a0c9d1624cc2d8b23adc3fac150fd91b8611ec
Peter Kim <161254279+peterkimpk1@users.noreply.github.com>
2025-02-10 19:11:37 -0500

    Added sessionId randomly generated for user to generate/validate CSRF token

--- src/lib/auth/session.ts
+++ src/lib/auth/session.ts
@@ -1,26 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { generateJWT, verifyJWT } from "./jwt";
 import { serialize } from "cookie";
+import { randomUUID } from 'crypto';
 
-interface createSessionProps {
-  res: NextApiResponse
-  userId: string,
-  secret: string
-}
-
-export const createSession = async ({res, userId, secret}:createSessionProps) => {
+export const createSession = async (res: NextApiResponse, userId: string, secret: string) => {
+  const sessionId = randomUUID();
   const token = await generateJWT({
-    payload: {userId},
+    payload: { userId },
     secret: secret,
     options: { expiresIn: '1h' }
   });
-  res.setHeader('Set-Cookie', serialize('auth', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge: 3600, // 1 hour
-    path: '/',
-  }));
+  res.setHeader('Set-Cookie', [
+    serialize('auth', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 3600, // 1 hour
+      path: '/',
+    }),
+    serialize('sessionId', sessionId, {
+      httpOnly: false,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 3600,
+      path: '/'
+    }),
+  ]);
 };
 
 export const endSession = (res: NextApiResponse) => {
