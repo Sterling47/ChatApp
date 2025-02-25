96b6d8c71a7b75a3363a53e9638d86ca45b66417
Peter Kim <161254279+peterkimpk1@users.noreply.github.com>
2025-02-25 16:30:02 -0500

    feat: Added JWT verification for GET friend routes

--- src/app/api/getFriends/route.ts
+++ src/app/api/getFriends/route.ts
@@ -1,9 +1,19 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db';
 import { getUser } from '@/lib/auth/user';
-export async function GET() {
+import { getSession } from '@/lib/auth/session';
+export async function GET(req: NextRequest) {
     try {
         const user = await getUser();
+        const secret = process.env.JWT_SECRET!
+        const authToken = req.cookies.get('auth')?.value
+        const session = await getSession(authToken, secret)
+        if (!session) {
+            return NextResponse.json(
+                { message: 'Invalid or expired session' },
+                { status: 401 }
+            )
+        }
         if (!user || !user.email) {
             return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
         }
@@ -35,14 +45,14 @@ export async function GET() {
             }
         });
 
-        return NextResponse.json({ 
+        return NextResponse.json({
             friends: friends.map(f => ({
                 id: f.friend.id,
                 email: f.friend.email,
                 username: f.friend.username,
                 isOnline: f.friend.isOnline,
                 friendshipId: f.id
-            })) 
+            }))
         }, { status: 200 });
     } catch (error) {
         console.error('Failed to fetch friends:', error);
--- src/app/api/search-user/route.ts
+++ src/app/api/search-user/route.ts
@@ -1,43 +1,53 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db';
 import { getUser } from '@/lib/auth/user';
-export async function GET(request: Request) {
+import { getSession } from '@/lib/auth/session';
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get('query');
   try {
     const user = await getUser();
-        if (!user || !user.email) {
-            return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-        }
+    const secret = process.env.JWT_SECRET!
+    if (!user || !user.email) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
 
-        const currentUser = await prisma.user.findUnique({
-            where: { email: user.email },
-        });
+    const authToken = request.cookies.get('auth')?.value
+    const session = await getSession(authToken, secret)
+    if (!session) {
+      return NextResponse.json(
+        { message: 'Invalid or expired session' },
+        { status: 401 }
+      )
+    }
+    const currentUser = await prisma.user.findUnique({
+      where: { email: user.email },
+    });
 
-        if (!currentUser) {
-            return NextResponse.json({ error: 'User not found' }, { status: 404 });
-        }
-        
+    if (!currentUser) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
     const users = await prisma.user.findMany({
       where: {
         AND: [
           query
             ? {
-                OR: [
-                  { username: { contains: query, mode: 'insensitive' } },
-                  { email: { contains: query, mode: 'insensitive' } },
-                ],
-              }
+              OR: [
+                { username: { contains: query, mode: 'insensitive' } },
+                { email: { contains: query, mode: 'insensitive' } },
+              ],
+            }
             : {},
           { id: { not: currentUser.id } },
-          { isGuest: false} 
+          { isGuest: false }
         ],
       },
       select: {
         id: true,
         username: true,
         email: true,
-        isOnline: true, 
+        isOnline: true,
       },
     });
 
