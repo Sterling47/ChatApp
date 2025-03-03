1f8e863a66e0fa6e9c66689cd59e9c8c8e585eb4
Adam Konber <konber3@gmail.com>
2025-03-03 16:28:14 -0700

    Updated registar modal and lifted state out and is now handled in app/page and passed as isClosed prop

--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -4,32 +4,26 @@ import React, { useState } from "react";
 import AuthForm from "./AuthForm";
 import { Button } from "@/components/ui/button";
 
-const RegisterModal = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+interface RegisterModalProps {
+  onClose: () => void;
+}
 
-  const toggleModal = () => setIsModalOpen(!isModalOpen);
+const RegisterModal = ({ onClose }: RegisterModalProps) => {
 
   return (
-    <div>
-
-      <Button className='login-btn mx-[3rem] z-20 hover:cursor-pointer bg-transparent hover:bg-transparent' onClick={toggleModal}>
-        <span>Sign In</span>
-      </Button>
-      {isModalOpen && (
-        <div className="fixed inset-0 z-50">
-          <div
-            className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm"
-            onClick={toggleModal}
-          ></div>
-          <div className="fixed inset-0 flex items-center justify-center">
-            <div className="bg-white p-6 rounded-lg">
-              <AuthForm toggleModal={toggleModal} />
-            </div>
+    <div data-testid='register-modal'>
+      <div className="fixed inset-0 z-50">
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm"
+          onClick={onClose}
+        ></div>
+        <div className="fixed inset-0 flex items-center justify-center">
+          <div className="bg-white p-6 rounded-lg">
+            <AuthForm toggleModal={onClose} /> 
           </div>
         </div>
-      )}
+      </div>
     </div>
-
   );
 };
 
