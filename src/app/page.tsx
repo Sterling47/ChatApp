1f6b55a66eddbb243639916e09a591fe86322875
Adam Konber <konber3@gmail.com>
2025-03-03 16:24:32 -0700

    Refactored app/page to handle state instead of the Login button component wraping Register modal and handling state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
+'use client'
+
 import './globals.css'
+import { useState } from 'react';
 import { BackgroundBeams } from '@/components/ui/background-beams';
 import { FlipWords } from '@/components/ui/flip-words';
 import RegisterModal from '@/components/RegisterModal';
 import { LoginButton } from '@/components/LoginButton';
 
-export default async function App() {
+export default function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const words = ['Create!', 'Learn!', 'Teach!', 'Translate!']
   
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
 
   return (
     <div className='flex justify-center flex-col items-start h-[100vh]' id='home'> 
         <h1 className='ml-12 text-[6rem]'>Welcome to Chatt-r</h1>
         <div className='m-[3rem] text-[2rem]'>Your place to <FlipWords words={words}/></div>
-        <LoginButton>
-          <RegisterModal/>
-        </LoginButton>
+        <LoginButton onClick={toggleModal} />
+          {isModalOpen && <RegisterModal onClose={toggleModal}/>}
       <BackgroundBeams />
     </div>
   );
