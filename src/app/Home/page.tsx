88682ffecd1bd7a226de4897b562d8a12f25ee97
Adam Konber <konber3@gmail.com>
2025-03-21 15:13:01 -0600

    Updated Home/Page styles

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
  const page = () => {
   return (
-    <div className='flex justify-center items-center flex-col m-0.5 gap-4 row-start-1 row-end-13 col-start-2 col-end-10'>
-        <h1>Welcome Page</h1>
+    <div className='flex justify-center rounded-xl h-full items-center flex-col  gap-4 '>
+        <h1>Welcome to Chatt-r</h1>
       </div>
   )
 }
