ae8a14dda365f55ca462c21a325f9b493a8bfeb1
Adam Konber <konber3@gmail.com>
2025-03-22 10:16:04 -0600

    Updated styles to room links, header and close button be more appealing and consistent throughout the app.

--- src/components/RoomModalforMobile.tsx
+++ src/components/RoomModalforMobile.tsx
@@ -49,13 +49,13 @@ const RoomModalforMobile: React.FC<RoomModalforMobileProps> = ({ initialRooms })
         <div className='row-start-1 row-end-3 md:hidden lg:hidden xl:hidden'>
             <button onClick={() => setIsOpen(true)} className="text-white rounded-md">Chat Rooms</button>
             {isOpen && (
-                <div className="fixed inset-0 z-10 flex items-center justify-center bg-black border bg-opacity-25">
-                    <div className="bg-white rounded-2xl w-[50%] p-6 shadow-xl">
-                        <h3 className="text-lg font-medium leading-6 text-gray-900">Rooms</h3>
-                        <div className="mt-2">
+                <div className="fixed inset-0 z-10 flex items-center justify-center bg-black  bg-opacity-50">
+                    <div className=" flex flex-col justify-between bg-primary rounded-2xl w-[60%] h-[50%] p-6 shadow-xl">
+                        <h3 className="text-lg font-medium leading-6 text-white">Rooms</h3>
+                        <div className="flex flex-col gap-3 mt-2 h-[80%]">
                             {rooms.map(renderRoomLink)}
                         </div>
-                        <button onClick={() => setIsOpen(false)} className="mt-4 text-white bg-red-500 rounded-md">Close</button>
+                        <button onClick={() => setIsOpen(false)} className="mt-4 text-white bg-slate-700 p-2 w-full rounded-md">Close</button>
                     </div>
                 </div>
             )}
