71b0435d1e8b0e9c9e9b094522d96794603a5cd9
Adam Konber <konber3@gmail.com>
2025-03-22 10:46:19 -0600

    Updated form styles and added a h3 to shows it the create room form. made changes to inputs and submit button.

--- src/components/CreateRoom.tsx
+++ src/components/CreateRoom.tsx
@@ -11,21 +11,26 @@ const CreateRoom = () => {
   }
 
   return (
-    <div className='fixed bottom-4 left-4 z-20'>
+    <div className='fixed bottom-4 left-2 z-20'>
       <Fab color="primary" aria-label="add" onClick={toggleModal}>
         <AddIcon />
       </Fab>
       {isModalOpen && 
-      (<div className="fixed inset-0 flex bg-coolGrey items-center justify-center z-21">
-        <div className="flex flex-col justify-evenly items-center bg-primary text-white rounded-lg  w-[40%] h-[50%] shadow-room">
+      (<div className="fixed inset-0 flex bg-black bg-opacity-50 items-center justify-center z-21">
+        <div className="flex flex-col justify-evenly items-center bg-primary text-white rounded-lg  w-[60%] h-[50%] shadow-room">
           <form className='flex flex-col h-[90%] w-[90%] justify-around items-start' action={async(formData: FormData) => {
             await createRoomAction(formData)
-          }}>
-              <label>Room Name:
-              <input type='text' name='room-name' className='bg-gray-500 border-none'></input></label>
-              <label>Private:
-              <input type='checkbox' name='private' className='bg-gray-500 border-none'></input></label>
-              <button type='submit' className='w-[30%] flex justify-center items-center p-0 bg-primary text-white'>Submit</button>
+          }}> 
+                <h3 className="text-lg font-semibold mb-4">Create a New Chat Room</h3>
+                <div className="mb-4 w-full">
+                <label className="block mb-2">Room Name:</label>
+                <input type='text' name='room-name' className='w-full p-2 bg-gray-500 border-none rounded-md'></input>
+                </div>
+                <div className="mb-4 w-full">
+                <label className="block mb-2">Private:</label>
+                <input type='checkbox' name='private' className='bg-gray-500 border-none'></input>
+                </div>
+                <button type='submit' className='w-full p-2 rounded-md bg-slate-700 text-white'>Submit</button>
           </form>
         </div>
       </div>
